f3083777657957b349a4644c9286968c82f57461
roarijo89 <roarijo89@gmail.com>
2020-05-25 20:49:00 -0400

    new

---
+++ subway/js/routes.js
@@ -0,0 +1,40 @@
+var routes = [
+  // Index page
+  {
+    path: '/',
+    url: './index.php',
+    name: 'home',
+  },
+  {
+    path: '/games/',
+    componentUrl: './pages/games.php',
+  },
+  {
+    path: '/hacked/',
+    componentUrl: './pages/hacked.php',
+  },
+  {
+    path: '/appstore/',
+    componentUrl: './appstore/appstore.php',
+  },
+  {
+    path: '/eonhub/',
+    componentUrl: './pages/eonhub.php',
+  },
+  {
+    path: '/jailbreak/',
+    componentUrl: './pages/jailbreak.php',
+  },
+  {
+    path: '/removed/',
+    componentUrl: './pages/removed.php',
+  },
+  {
+    path: '/tweaked/',
+    componentUrl: './pages/tweaked.php',
+  },
+  {
+    path: '/betas/',
+    componentUrl: './pages/betas.php',
+  },
+];
